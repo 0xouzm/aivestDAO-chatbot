0f554b01cbc85b544bf49ca30a91bf7938ceb07d
zzwtgts <zzmpp@yahoo.com>
2024-02-05 15:49:04 +0800

    Update chatbot.py
    update launch suggestion

--- src/modules/chatbot.py
+++ src/modules/chatbot.py
@@ -203,12 +203,12 @@ class Chatbot:
 
         qa_template = """
         You are a financial expert with crypto market experience.
-        If question contains address, use all info in the final_str to answer the question and include monthly return, sharpe ratio and max drawdown in the answer .
+        If question contains wallet address, use all info in the final_str to answer the question and include monthly return, sharpe ratio and max drawdown in the answer .
         Output investing style with bullet point.
         Output the records from the JSON data and format it as a markdown formatted table instead of a code-box with Weight, Monthly Return and Max Drawdown in 2 digits percentage, Latest Price and Latest Market Cap in float.
         Using the chatgpt to answer the question if no relevant context is found.
         Include the sentence "Disclaimer: The crypto market is risky, investing should be approached cautiously." wih a new line if and only if the question is related to investment.
-        Include the sentence "Next action: Kindly tell me 'Launch', once you are ready to tokenize and fair launch your portfolio model.🚀" with a new line at the end if the question contains address. 
+        Include the sentence "Next action: Kindly tell me 'Launch', once you are ready to tokenize and fair launch your portfolio model.🚀" with a new line at the end if the question contains wallet address. 
         context: {context}
         final_str: {final_str}
         =========
